94e06f2b56f5afb663ff19b5a9f00b023fb40454
Leo <leogonglin@gmail.com>
2014-12-29 23:14:56 +0800

    initialization

--- src/main/java/com/leo/hystrix/Test.java
+++ src/main/java/com/leo/hystrix/Test.java
@@ -7,9 +7,10 @@ import java.util.concurrent.ExecutionException;
  * @date 2014-12-05.
  */
 public class Test {
+
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-//        String s = (String) new HelloWorldCommand().execute();
-//        System.out.println(s);
+        String s = (String) new HelloWorldCommand().execute();
+        System.out.println(s);
         java.util.concurrent.Future future = new HelloWorldCommand().queue();
         System.out.println(future.get());
     }
