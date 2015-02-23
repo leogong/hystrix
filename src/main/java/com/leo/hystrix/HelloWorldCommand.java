31aed4daa62a19372e097db01aabee63e02117f7
Leo <leogonglin@gmail.com>
2015-02-23 17:19:13 +0800

    classloader

--- src/main/java/com/leo/hystrix/HelloWorldCommand.java
+++ src/main/java/com/leo/hystrix/HelloWorldCommand.java
@@ -7,16 +7,23 @@ import com.netflix.hystrix.HystrixCommandGroupKey;
  * @auther leo
  * @date 2014-12-05.
  */
-public class HelloWorldCommand extends HystrixCommand {
+public class HelloWorldCommand extends HystrixCommand<String> {
+
+    String name;
 
     public HelloWorldCommand() {
         super(HystrixCommandGroupKey.Factory.asKey("MyGroup"));
     }
 
+    public HelloWorldCommand(String name) {
+        super(HystrixCommandGroupKey.Factory.asKey("ExampleGroup"));
+        this.name = name;
+    }
+
     @Override
     protected String run() {
         try {
-            Thread.sleep(1000*2);
+            Thread.sleep(1000 * 2);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
--- src/main/java/com/leo/hystrix/Test.java
+++ src/main/java/com/leo/hystrix/Test.java
@@ -1,5 +1,9 @@
 package com.leo.hystrix;
 
+import rx.Observable;
+import rx.Observer;
+import rx.util.functions.Action1;
+
 import java.util.concurrent.ExecutionException;
 
 /**
@@ -9,9 +13,30 @@ import java.util.concurrent.ExecutionException;
 public class Test {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        String s = (String) new HelloWorldCommand().execute();
-        System.out.println(s);
-        java.util.concurrent.Future future = new HelloWorldCommand().queue();
-        System.out.println(future.get());
+        // String s = (String) new HelloWorldCommand().execute();
+        // System.out.println(s);
+        // java.util.concurrent.Future future = new HelloWorldCommand().queue();
+        // System.out.println(future.get());
+        Observable<String> fs = new HelloWorldCommand("World").observe();
+        fs.subscribe(new Observer<String>() {
+
+            @Override
+            public void onCompleted() {
+                System.out.println("onCompleted");
+                // nothing needed here
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                System.out.println("onError");
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onNext(String v) {
+                System.out.println("onNext: " + v);
+            }
+
+        });
     }
 }
